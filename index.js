edb89efe36def235c8c59e7a1e333429372be8e0
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-30 21:54:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-window.addEventListener("load", () => {
-  //start your code here
-  let outputResult = document.querySelector("output")
-  let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".split('')
-  let upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
-  let numberCharacters = "0123456789".split('')
-  let specialCharacters = "!#$%&()*+,-./:;<=>?@[\\]^_{|}~".split('')
+// window.addEventListener("load", () => {
+//   //start your code here
+//   let outputResult = document.querySelector("output")
+//   let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".split('')
+//   let upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+//   let numberCharacters = "0123456789".split('')
+//   let specialCharacters = "!#$%&()*+,-./:;<=>?@[\\]^_{|}~".split('')
 
-  document.addEventListener('submit', (event) => {
-    // stay on the same page
-    event.preventDefault();
+//   document.addEventListener('submit', (event) => {
+//     // stay on the same page
+//     event.preventDefault();
     
-    let input = document.getElementById("passwordLength")
-    let passwordLength = input.value
-    if (passwordLength < 4) {
-      passwordLength = 4
-    }
-    let newPassword = ""
-    for (let i = 0; i < passwordLength;) {
-      if (i < passwordLength) {
-      newPassword += lowerCaseCharacters[Math.floor(Math.random() * lowerCaseCharacters.length)]
-        i++
-      }
-      if (i < passwordLength) {
-      newPassword += upperCaseCharacters[Math.floor(Math.random() * upperCaseCharacters.length)]
-        i++
-      }
-      if (i < passwordLength) {
-      newPassword += numberCharacters[Math.floor(Math.random() * numberCharacters.length)]
-        i++
-      }
-      if (i < passwordLength) {
-      newPassword += specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
-        i++
-      }
+//     let input = document.getElementById("passwordLength")
+//     let passwordLength = input.value
+//     if (passwordLength < 4) {
+//       passwordLength = 4
+//     }
+//     let newPassword = ""
+//     for (let i = 0; i < passwordLength;) {
+//       if (i < passwordLength) {
+//       newPassword += lowerCaseCharacters[Math.floor(Math.random() * lowerCaseCharacters.length)]
+//         i++
+//       }
+//       if (i < passwordLength) {
+//       newPassword += upperCaseCharacters[Math.floor(Math.random() * upperCaseCharacters.length)]
+//         i++
+//       }
+//       if (i < passwordLength) {
+//       newPassword += numberCharacters[Math.floor(Math.random() * numberCharacters.length)]
+//         i++
+//       }
+//       if (i < passwordLength) {
+//       newPassword += specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+//         i++
+//       }
       
-    }
-   outputResult.innerHTML = newPassword
-  })
+//     }
+//   outputResult.innerHTML = newPassword
+//   })
   
-})
+// })
